317f2d8f8607bdb0485a8dd76bac8fffa4203850
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-07-29 06:28:46 +0100

    chore: initial

---
+++ StaminaDebug/StaminaDebug/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using API;
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace StaminaDebug.BepInEx;
+
+[BepInPlugin(Module.GUID, Module.Name, Module.Version)]
+public class Plugin : BasePlugin {
+    public override void Load() {
+        APILogger.Log("Plugin is loaded!");
+        harmony = new Harmony(Module.GUID);
+        harmony.PatchAll();
+
+        APILogger.Log("Debug is " + (ConfigManager.Debug ? "Enabled" : "Disabled"));
+
+        RundownManager.OnExpeditionGameplayStarted += (Action)(() => {
+            PlayerStamina.EnableStaminaDebugGraph = true;
+        });
+    }
+
+    private static Harmony? harmony;
+}
